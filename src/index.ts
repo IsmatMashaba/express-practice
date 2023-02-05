d180fbd034b4ad2f0e8c1e69e18659663dfdcd5d
Ismat Mashaba <ismat.mashaba96@gmail.com>
2023-02-05 21:15:29 +0100

    ready to review

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,45 @@
-import * as express_ from 'express'
-/*import express, {Express} from 'express'*/
-/*import express = require('express');*/
-import {Request,Response} from "express";
+import Express, {Request,Response} from 'express'
 
-let express = express_  // *-* MaGiC Happens Here *-*
-let app = express()
-/*data- we can read*/
-app.use(express.json())
+//fake database
+const userList = [
+    {id: 1, name: 'Andrea', age:3},
+    {id: 2, name: 'Ismat', age:4},
+    {id: 3, name: 'Thoa', age:5},
+];
 
-/*request frontend to backend: 1.method 2. endpoint*/
-/*methods- post,get,delete,put,patch*/
+const app=Express();
+app.use(Express.json())
 
-/*1. method: get , endpoint:http://localhost:8000/ */
-/*inside get method there are 2 things i. the endpoint and ii. arrow function to deal with req and res*/
+//get method to get all the user
+app.get("/users",(req:Request,res:Response)=>{
+    res.json (userList)
+})
 
-app.get("/", (req:Request,res:Response) => {
-res.json('this is get method')
-});
+//post method to add new user  {id: 4, name: 'Soo', age:5}
+app.post("/users",(req:Request,res:Response)=>{
+    const newUser = req.body  //body is from postman
+    userList.push(newUser)
+    res.json (userList)
+})
+
+//delete method endpoint: http://localhost:8000/users/1
+//to make it dinamic the endpint should be:http://localhost:8000/users/:id 
+
+//app.delete("/users/:id",(req:Request,res:Response)=>{
+   // console.log(req.params)   we always get string in params method
+   // res.json ('this is delete method')
+//} )
+
+//delet specific id 
+app.delete("/users/:id",(req:Request,res:Response)=>{
+    //first to get the specific id by params and then remove the id from array
+    const userId = req.params;  //it's an object
+   const result= userList.filter((user)=> user.id!== Number(userId.id))
+        res.json (result)
+})
+
 
 const port= 8000;
-/*endpoint:http://localhost:8000/*/
 
 app.listen(port, ()=>{
     console.log(`the server is running on http://localhost:$(port)`)
